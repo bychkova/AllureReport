4ad061033e19b599d67ac574b75759ae6b321552
Maria Bychkova <mar.byckova@gmail.com>
2022-09-30 15:49:29 +0300

    Modified test

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -28,7 +28,7 @@ class DeliveryTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 
