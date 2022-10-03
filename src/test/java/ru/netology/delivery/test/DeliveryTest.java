839a1226180b830dd0fbdc70d4f2330bd20c2263
Maria Bychkova <mar.byckova@gmail.com>
2022-10-03 11:24:43 +0300

    Changed COMMAND to CONTROL

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -36,7 +36,7 @@ class DeliveryTest {
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
 
         $("[data-test-id='city'] input").setValue(validUser.getCity());
-        $("[data-test-id='date'] input").sendKeys(Keys.COMMAND + "A");
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] input").sendKeys(BACK_SPACE);
         $("[data-test-id='date'] input").setValue(firstMeetingDate);
         $("[data-test-id='name'] input").setValue(validUser.getName());
@@ -45,7 +45,7 @@ class DeliveryTest {
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
 
-        $("[data-test-id='date'] input").sendKeys(Keys.COMMAND + "A");
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] input").sendKeys(BACK_SPACE);
         $("[data-test-id='date'] input").setValue(secondMeetingDate);
         $x("//span[contains(text(), 'Запланировать')]").click();
